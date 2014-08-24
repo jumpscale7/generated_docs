2a20f892d81b7202d8b3d16b550066eacfe412ca
Reem Khamis <khamisr@codescalers.com>
2014-08-24 13:00:49 +0300

    skip ci

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,10 +20,7 @@ import os
 sys.path.append("/opt/code/github/jumpscale/jumpscale_core/lib/JumpScale/")
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if on_rtd:
-    html_theme = 'default'
-else:
-    html_theme = 'nature'
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
